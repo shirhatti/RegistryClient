2f3e50ab64c9d3929ebd33ecbb9bcad4b34b959e
Sourabh Shirhatti <shirhatti@outlook.com>
2018-03-19 01:37:16 -0700

    Add Azure AD webapp

---
+++ sample/RegistryWebApp/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using RegistryWebApp.Models;
+
+namespace RegistryWebApp.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly AzureAdOptions _azureAdOptions;
+
+        public HomeController(IOptions<AzureAdOptions> azureAdOptions)
+        {
+            _azureAdOptions = azureAdOptions.Value;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ sample/RegistryWebApp/Helpers/AcrAuthProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistryWebApp.Helpers
+{
+    public class AcrAuthProvider
+    {
+        private readonly IMemoryCache _memoryCache;
+        private readonly string _appId;
+        //private readonly ClientCredential _credential;
+        public AcrAuthProvider(IMemoryCache memoryCache, IConfiguration configuration)
+        {
+            var azureOptions = new AzureAdOptions();
+            configuration.Bind("AzureAd", azureOptions);
+
+            _appId = azureOptions.ClientId;
+            //_credential = new ClientCredential(azureOptions.ClientSecret);
+
+            _memoryCache = memoryCache;
+        }
+    }
+}
